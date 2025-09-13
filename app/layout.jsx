0b380cfc7ba2079f7da1c2be77e73cf8b9a4cf3c
YL2270 <innards_stable7x@icloud.com>
2025-09-13 18:10:40 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,17 +18,40 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* 在這裡貼上你的 GA4 追蹤程式碼 */}
-        <!-- Google tag (gtag.js) -->
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-7K4GH0T9MV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        import { Inter } from "next/font/google";
+import "./globals.css";
 
-  gtag('config', 'G-7K4GH0T9MV');
-</script>
-        </head>
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Yilungc",
+  description: "Dive into insightful book reviews or explore captivating UK life experiences.",
+  icons: {
+    icon: [
+      { url: 'images/favicon.png', type: 'image/png', sizes: '512x512' },
+    ],
+    apple: 'images/favicon.png'
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* 在這裡貼上你的 GA4 追蹤程式碼 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7K4GH0T9MV"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7K4GH0T9MV');
+            `,
+          }}
+        />
+      </head>
+ 
       <body className={inter.className}>{children}</body>
     </html>
   )
